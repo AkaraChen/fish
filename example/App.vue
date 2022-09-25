6fcb9391df6ffc075bd919b4caeb71e8360001ca
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-09-25 19:45:44 +0800

    🦄 refactor: add var prefix

--- example/App.vue
+++ example/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="max-width: 800px; margin: auto">
+    <div style="max-width: 800px; margin: auto; padding: 10px 20px">
         <header>
             <hgroup>
                 <router-link to="/">
